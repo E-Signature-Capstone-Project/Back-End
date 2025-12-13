19fa7b21b5a517f57f0631ac2a3c27a89d72738c
Awalrmdhn <11221067@student.itk.ac.id>
2025-12-13 19:07:07 +0800

    add regis admin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const { searchUsers } = require("../controllers/userController");
 
+// ini buat cari user berdasarkan query yang ada di db jadi kaya search user gitu kalo mau minta ttd kaya gmail 
 router.get("/search", authMiddleware, searchUsers);
 
 module.exports = router;
