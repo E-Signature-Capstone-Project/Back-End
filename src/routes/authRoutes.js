3b27026b5729e44bb5a75391fd0139d567be812c
Awalrmdhn <11221067@student.itk.ac.id>
2025-12-13 18:20:52 +0800

    add route admin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,6 +9,12 @@ const {
   updateName
 } = require("../controllers/authController");
 
+const { 
+  createAdmin, 
+  approveAdmin, 
+  rejectAdmin  
+} = require("../controllers/adminController");
+
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Public
@@ -21,5 +27,12 @@ router.put("/name", authMiddleware, updateName);
 
 // Dev: create admin (optional protection if needed)
 router.post("/create-admin", createAdmin);
+
+// Approve admin
+router.put("/approve/:id", authMiddleware, isAdmin, approveAdmin);
+
+// Reject admin
+router.put("/reject/:id", authMiddleware, isAdmin, rejectAdmin);
+
 
 module.exports = router;
